f7ff7faa109f69bb1d94218519048dbdbafba5d8
Thiago de Jesus <99thiagodejesus@gmail.com>
2020-12-17 16:28:03 -0300

    Test Code to Instance at Cannon

--- Objects/Cannon.gd
+++ Objects/Cannon.gd
@@ -2,12 +2,18 @@ extends Node2D
 
 var rot: float = 0
 var rot_speed: int = 1
+var object_on: bool = false
+var object_preloads: = [preload("res://Objects/PeçaReta.tscn"), 
+preload("res://Objects/Quadrado.tscn")]
 
 func _physics_process(delta: float) -> void:
 	cannon_rotation()
 	$SpriteCannon.rotate(rot * rot_speed * delta)
 	convert_degree()
-
+	if not object_on:
+		object_instance()
+	
+	
 func convert_degree () -> void:
 	if $SpriteCannon.rotation_degrees > 360:
 		$SpriteCannon.rotation_degrees = 0
@@ -17,4 +23,9 @@ func convert_degree () -> void:
 func cannon_rotation() -> float:
 	rot = Input.get_action_strength("right1") - Input.get_action_strength("left1")
 	return rot
+	
+func object_instance() -> void:
+	object_on = true
+	var object = object_preloads[1].instance()
+	self.add_child(object)
 	
