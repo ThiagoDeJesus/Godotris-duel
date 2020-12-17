83779f504c22f58b9cf29b4219181bc72293b726
Thiago de Jesus <99thiagodejesus@gmail.com>
2020-12-17 17:27:26 -0300

    Added Construction view in front of cannon

--- Objects/Cannon.gd
+++ Objects/Cannon.gd
@@ -4,10 +4,13 @@ var rot: float = 0
 var rot_speed: int = 1
 var object_on: bool = false
 var construction_preloads: = [preload("res://Objects/constructions/PeçaCurva.tscn"), 
-preload("res://Objects/constructions/PeçaL.tscn"), 
+preload("res://Objects/constructions/PecaL.tscn"), 
 preload("res://Objects/constructions/PeçaReta.tscn"),
 preload("res://Objects/constructions/PeçaSetas.tscn"),
 preload("res://Objects/constructions/Quadradao.tscn")]
+var construction
+var construction_pos: Vector2
+var nodename: String
 
 func _physics_process(delta: float) -> void:
 	cannon_rotation()
@@ -15,7 +18,8 @@ func _physics_process(delta: float) -> void:
 	convert_degree()
 	if not object_on:
 		object_instance()
-	
+	if object_on:
+		pre_shoot()
 	
 func convert_degree () -> void:
 	if $SpriteCannon.rotation_degrees > 360:
@@ -29,7 +33,13 @@ func cannon_rotation() -> float:
 	
 func object_instance() -> void:
 	object_on = true
-	var construction = construction_preloads[1].instance()
-	var construction_pos = $SpriteCannon/MuzzleCannon.global_position
-	construction.global_position = construction_pos
-	$"../".add_child(construction)
+	construction = construction_preloads[1].instance()
+	construction_pos = $SpriteCannon/MuzzleCannon.position
+	construction.position = construction_pos
+	construction.mode = 3
+	nodename = construction.name
+	self.add_child(construction)
+
+func pre_shoot() ->void:
+	construction_pos = $SpriteCannon/MuzzleCannon.global_position
+	get_node(nodename).global_position = construction_pos
