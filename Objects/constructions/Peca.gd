2be41c5996e780bb8cfa67bfec92370493743a01
Thiago de Jesus <99thiagodejesus@gmail.com>
2020-12-23 00:23:25 -0300

    Grid moviment part 1

--- Objects/constructions/Peca.gd
+++ Objects/constructions/Peca.gd
@@ -1,59 +1,14 @@
 extends RigidBody2D
 
-var rot: float
-var naarea: bool = false
-var rodado: bool = false
+var arealeftlimit: = 279
+var arearightlimit: = 359
+var outofplace = true
 
 func _physics_process(delta: float) -> void:
-	if self.mode != 0:
-		set_physics_process(false)
-	convert_degree()
-	if linear_velocity == Vector2.ZERO and global_rotation_degrees != rot and not rodado and naarea:
-		land_degree()
-		rodado = true
-		
-func convert_degree() -> void:
-	if global_rotation_degrees > 360:
-		global_rotation_degrees = 0
-	if global_rotation_degrees < 0:
-		global_rotation_degrees = 360
-		
-func land_degree() ->void:
-	var dif: = []
-	var teste: float
-	dif.append(int(abs(global_rotation_degrees - 90)))
-	dif.append(int(abs(global_rotation_degrees - 180)))
-	dif.append(int(abs(global_rotation_degrees - 270)))
-	dif.append(int(abs(global_rotation_degrees - 360)))
-	dif.append(int(abs(global_rotation_degrees - 0)))
-	for c in dif.size():
-		teste = global_rotation_degrees + dif[c]
-		if (teste >= -5 and teste <= 5) or (teste >= 85 and teste <= 95) or (teste >= 175 and teste <= 185) or (teste >= 265 and teste <= 275) or (teste >= 355 and teste <= 360):
-			rot = teste
-		c += 1
-	while global_rotation_degrees != rot:
-		if global_rotation_degrees < rot:
-			rotate(get_physics_process_delta_time() * 10)
-		if global_rotation_degrees > rot:
-			rotate(get_physics_process_delta_time() * -10)
-
-
-func _on_PecaL_body_entered(body: Node) -> void:
-	if body.is_in_group("detector"):
-		naarea = true
-
-func _on_PeaCurva_body_entered(body: Node) -> void:
-	if body.is_in_group("detector"):
-		naarea = true
-
-func _on_PeaReta_body_entered(body: Node) -> void:
-	if body.is_in_group("detector"):
-		naarea = true
-
-func _on_PeaSetas_body_entered(body: Node) -> void:
-	if body.is_in_group("detector"):
-		naarea = true
-
-func _on_Quadradao_body_entered(body: Node) -> void:
-	if body.is_in_group("detector"):
-		naarea = true
+	if linear_velocity <= Vector2(.01,.01) and linear_velocity > Vector2(-.01,-.01) and global_position.x >= arealeftlimit and global_position.x <= arearightlimit and outofplace:
+		rotation_degrees = 90
+		global_position = global_position.snapped(Vector2(8, 8))
+		outofplace = false
+		mode = 2
+		print(linear_velocity)
+	
